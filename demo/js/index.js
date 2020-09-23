f8f7b981230ef7f12d2ff7f30b748198f3396052
Riya <hiyoriorria@gmail.com>
2020-09-23 23:07:39 +0900

    微細な変更

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,3 +1,3 @@
 import FCC from './../../src/FCC'
 import './../scss/index.scss'
-FCC.Html.html("root", `<h1>FCCの使い方</h1><p>基本</p><a href="./Math.html">a</a>`)
+FCC.Html.html("root", `<h1>FCCの使い方</h1><p>準備</p><a href="./Math.html">Math</a>`)
