f719198ed712a910a0bdd98c507f562b4c3698a9
Riya <hiyoriorria@gmail.com>
2020-09-24 15:12:57 +0900

    JSのコードの色つけ

--- demo/js/Html.js
+++ demo/js/Html.js
@@ -1,1 +1,14 @@
 import FCC from './../../src/FCC'
+import './../scss/index.scss'
+import $ from 'jquery'
+FCC.Html.jscode(
+    "root",
+    '<input type="text" id="html"/><div id="code"></div>'
+)
+$('#html').keyup(function ()
+{
+    FCC.Html.jscode(
+        "code",
+        `<div class="jscode">${$('#html').val()}</div>`
+    )
+})
