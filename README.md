8c5b4b12f156cc4aa83c25b759b2aa7deb507007
Riya <hiyoriorria@gmail.com>
2020-10-27 11:37:29 +0900

    微細な変更

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 **React の場合**
 
 ```
-import FCC from './react/FCC'
+import FCC from './react/FCC' or npm i fcc_react_modules
 ```
 
 **Rails の場合**
--- src/FCC-Modules/FCC@1.3.js
+++ src/FCC-Modules/FCC@1.3.js
@@ -9,16 +9,16 @@ import maths from './function/Math/Math';
 import Code from './function/code/Code';
 
 const Search = (req, reqid) => {
-	const getid = document.getElementById(reqid)
-	getid.innerHTML = Sciences(req);
+    const getid = document.getElementById(reqid)
+    getid.innerHTML = Sciences(req);
 }
 const Modal_simple = (req, reqid) => {
-	const getid = document.getElementById(reqid)
-	getid.innerHTML = ModalSimple(req);
+    const getid = document.getElementById(reqid)
+    getid.innerHTML = ModalSimple(req);
 }
 const Aimg = (req, reqid) => {
-	const getid = document.getElementById(reqid)
-	getid.innerHTML = `<div class=${req.divcass} id=${req.divid}>
+    const getid = document.getElementById(reqid)
+    getid.innerHTML = `<div class=${req.divcass} id=${req.divid}>
 		<a
 		href=${req.url}
 		id=${req.aid}
@@ -34,8 +34,8 @@ const Aimg = (req, reqid) => {
 	</div>`
 }
 const Accordion = (req, reqid) => {
-	const getid = document.getElementById(reqid)
-	getid.innerHTML = `<div>
+    const getid = document.getElementById(reqid)
+    getid.innerHTML = `<div>
 				<ul class="accordion-tabs-minimal">
 					<li class="tab-header-and-content">
 						<a href="#" class="tab-link is-active">${req.title1}</a>
@@ -66,15 +66,15 @@ const Accordion = (req, reqid) => {
 
 }
 const Thedetails = (req, reqid) => {
-	const getid = document.getElementById(reqid)
-	getid.innerHTML = `<div>
+    const getid = document.getElementById(reqid)
+    getid.innerHTML = `<div>
 				<p>Producer: ${req.producer}</p>
 				<p>Programming languages: ${req.programming}</p>
 			</div>`
 }
 const Sns = () => {
-	const getid = document.getElementById(reqid)
-	getid.innerHTML = `<div class="btn-sns">
+    const getid = document.getElementById(reqid)
+    getid.innerHTML = `<div class="btn-sns">
 				<a href=${req.url} class=${req.classs} btn-hover>
 					<span class=${req.span}>
 						<i class=${req.i}></i>
@@ -82,133 +82,141 @@ const Sns = () => {
 			</div>`;
 }
 class css {
-	constructor() {
-		this.Colorid = (id, color) => {
-			let Htmlcolor = document.getElementById(id)
-			Htmlcolor.style.color = color
-		}
-		this.Colorclass = (classs, color) => {
-			let Htmlcolor = document.getElementsByClassName(classs)
-			Htmlcolor.style.color = color
-		}
-	}
+    constructor() {
+        this.Colorid = (id, color) => {
+            let Htmlcolor = document.getElementById(id)
+            Htmlcolor.style.color = color
+        }
+        this.Colorclass = (classs, color) => {
+            let Htmlcolor = document.getElementsByClassName(classs)
+            Htmlcolor.style.color = color
+        }
+    }
 }
 const Random = (num) => {
-	return Math.floor(Math.random() * num);
+    return Math.floor(Math.random() * num);
 };
+const birthday = (y, m, d) => {
+    const birthday = new Date(y, m - 1, d);
+    const now = new Date()
+    const yb = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate())
+    const y = now.getFullYear() - birthday.getFullYear()
+    const show = (now < yb) ? y - 1 : y;
+    return show
+}
 const nCr = (n = 1, r = 1) => {
-	//nCr
-	let x, z;
-	let y;
-	let age = 1;
-	y = n;
-	z = r;
-	x = 0;
-	while (x == 0) {
-		if (r == 1) {
-			y = y * r;
-		}
-		r--;
-		if (r == 0) {
-			x++;
-		} else {
-			n--;
-			if (n == 0) {
-				x++;
-				break;
-			}
-			y = y * n;
-		}
-	}
-	for (let i = 2; i <= z; i++) {
-		age *= i;
-	}
-	y = y / age;
-	return y;
+    //nCr
+    let x, z;
+    let y;
+    let age = 1;
+    y = n;
+    z = r;
+    x = 0;
+    while (x == 0) {
+        if (r == 1) {
+            y = y * r;
+        }
+        r--;
+        if (r == 0) {
+            x++;
+        } else {
+            n--;
+            if (n == 0) {
+                x++;
+                break;
+            }
+            y = y * n;
+        }
+    }
+    for (let i = 2; i <= z; i++) {
+        age *= i;
+    }
+    y = y / age;
+    return y;
 }
 const Pow = (num, n) => {
-	//xのy乗
-	let nums = num;
-	for (let i = 2; i <= n; i++) {
-		nums *= num;
-	}
-	return nums;
+    //xのy乗
+    let nums = num;
+    for (let i = 2; i <= n; i++) {
+        nums *= num;
+    }
+    return nums;
 }
 const Repetition = (x = 1) => {
-	//階乗
-	let age = 1;
-	for (let i = 2; i <= x; i++) {
-		age *= i;
-	}
-	return age;
+    //階乗
+    let age = 1;
+    for (let i = 2; i <= x; i++) {
+        age *= i;
+    }
+    return age;
 }
 const nPr = (n, r) => {
-	//nPr
-	let y;
-	let x = 0;
-	y = n;
-	while (x == 0) {
-		if (r == 1) {
-			y = y * r;
-		}
-		r--;
-		if (r == 0) {
-			x++;
-		} else {
-			n--;
-			if (n == 0) {
-				x++;
-				break;
-			}
-			y = y * n;
-		}
-	}
-	return y;
+    //nPr
+    let y;
+    let x = 0;
+    y = n;
+    while (x == 0) {
+        if (r == 1) {
+            y = y * r;
+        }
+        r--;
+        if (r == 0) {
+            x++;
+        } else {
+            n--;
+            if (n == 0) {
+                x++;
+                break;
+            }
+            y = y * n;
+        }
+    }
+    return y;
 }
 const Gcd = (f = 1, x = 1) => {
-	//最大公約数
-	let r, tmp;
-	if (f < x) {
-		tmp = f;
-		f = x;
-		x = tmp;
-	}
-	/* ユークリッドの互除法 */
-	r = f % x;
-	while (r != 0) {
-		f = x;
-		x = r;
-		r = f % x;
-	}
-	return x;
+    //最大公約数
+    let r, tmp;
+    if (f < x) {
+        tmp = f;
+        f = x;
+        x = tmp;
+    }
+    /* ユークリッドの互除法 */
+    r = f % x;
+    while (r != 0) {
+        f = x;
+        x = r;
+        r = f % x;
+    }
+    return x;
 }
 const Ratio = (a = 1, b = 1, x = 1, y = 1) => {
-	//比率
-	a *= y;
-	b *= x;
-	if (a < b) {
-		if ((b % a) == 0) {
-			return b / a;
-		} else {
-			return `${a}/${b}`;
-		}
-	} else {
-		if ((a % b) == 0) {
-			return a / b;
-		} else {
-			return `${b}/${a}`;
-		}
-	}
+    //比率
+    a *= y;
+    b *= x;
+    if (a < b) {
+        if ((b % a) == 0) {
+            return b / a;
+        } else {
+            return `${a}/${b}`;
+        }
+    } else {
+        if ((a % b) == 0) {
+            return a / b;
+        } else {
+            return `${b}/${a}`;
+        }
+    }
 }
 const Diagonal = (n) => {
-	//対角線の数
-	n = (n * (n - 3)) / 2;
-	return n;
+    //対角線の数
+    n = (n * (n - 3)) / 2;
+    return n;
 }
 const Maths = new maths();
 const Mathsnew = () => {
-	let math = new maths();
-	return math;
+    let math = new maths();
+    return math;
 }
 const Css = new css();
 /*let Cssnew = () => {
@@ -216,131 +224,132 @@ const Css = new css();
 	return cs;
 }*/
 const Zeller = (y = 2000, m = 1, d = 1, lan = jp) => {
-	if (m < 3) {
-		y--;
-		m += 12;
-	}
-	let w = (y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) + Math.floor((13 * m + 8) / 5) + d) % 7;
-	if (w == 0) {
-		if (lan = en) {
-			w = "Sunday"
-		} else if (lan = jp) {
-			w = "日曜日";
-		}
-	} else if (w == 1) {
-		if (lan = en) {
-			w = "Manday"
-		} else if (lan = jp) {
-			w = "月曜日";
-		}
-	} else if (w == 2) {
-		if (lan = en) {
-			w = "Tuesday"
-		} else if (lan = jp) {
-			w = "火曜日";
-		}
-	} else if (w == 3) {
-		if (lan = en) {
-			w = "Wedneday"
-		} else if (lan = jp) {
-			w = "水曜日";
-		}
-	} else if (w == 4) {
-		if (lan = en) {
-			w = "Thursday"
-		} else if (lan = jp) {
-			w = "木曜日";
-		}
-	} else if (w == 5) {
-		if (lan = en) {
-			w = "Friday"
-		} else if (lan = jp) {
-			w = "金曜日";
-		}
-	} else {
-		if (lan = en) {
-			w = "Saturday"
-		} else if (lan = jp) {
-			w = "土曜日";
-		}
-	}
-	return w;
+    if (m < 3) {
+        y--;
+        m += 12;
+    }
+    let w = (y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) + Math.floor((13 * m + 8) / 5) + d) % 7;
+    if (w == 0) {
+        if (lan = en) {
+            w = "Sunday"
+        } else if (lan = jp) {
+            w = "日曜日";
+        }
+    } else if (w == 1) {
+        if (lan = en) {
+            w = "Manday"
+        } else if (lan = jp) {
+            w = "月曜日";
+        }
+    } else if (w == 2) {
+        if (lan = en) {
+            w = "Tuesday"
+        } else if (lan = jp) {
+            w = "火曜日";
+        }
+    } else if (w == 3) {
+        if (lan = en) {
+            w = "Wedneday"
+        } else if (lan = jp) {
+            w = "水曜日";
+        }
+    } else if (w == 4) {
+        if (lan = en) {
+            w = "Thursday"
+        } else if (lan = jp) {
+            w = "木曜日";
+        }
+    } else if (w == 5) {
+        if (lan = en) {
+            w = "Friday"
+        } else if (lan = jp) {
+            w = "金曜日";
+        }
+    } else {
+        if (lan = en) {
+            w = "Saturday"
+        } else if (lan = jp) {
+            w = "土曜日";
+        }
+    }
+    return w;
 }
 const Push = (array, text) => {
-	let Pushs = array.push(text);
-	return Pushs
+    let Pushs = array.push(text);
+    return Pushs
 }
 const Com = (num, num2) => {
-	let gcdnum = Gcd(3, 4);
-	num = (num * num2) / gcdnum;
-	return num;
+    let gcdnum = Gcd(3, 4);
+    num = (num * num2) / gcdnum;
+    return num;
 }
 const Spt = (num = 1, num2 = 1) => {
-	num2 = num + num2;
-	num *= 100;
-	return num / num2;
+    num2 = num + num2;
+    num *= 100;
+    return num / num2;
 }
 const Day = new Date();
 const hello = () => {
-	console.log("Welcome to FCC You can take the tutorial by accessing this URL");
-	console.log("https://riya81.github.io/FCC/");
+    console.log("Welcome to FCC You can take the tutorial by accessing this URL");
+    console.log("https://riya81.github.io/FCC/");
 }
 
 function sleep(waitSec, callbackFunc) {
 
-	var spanedSec = 0;
+    var spanedSec = 0;
 
-	var waitFunc = function () {
+    var waitFunc = function() {
 
-		spanedSec++;
+        spanedSec++;
 
-		if (spanedSec >= waitSec) {
-			if (callbackFunc) callbackFunc();
-			return;
-		}
+        if (spanedSec >= waitSec) {
+            if (callbackFunc) callbackFunc();
+            return;
+        }
 
-		clearTimeout(id);
-		id = setTimeout(waitFunc, 1000);
+        clearTimeout(id);
+        id = setTimeout(waitFunc, 1000);
 
-	};
+    };
 
-	var id = setTimeout(waitFunc, 1000);
+    var id = setTimeout(waitFunc, 1000);
 
 }
 export {
-	sleep,
-	hello,
-	Modal_simple,
-	Accordion,
-	Thedetails,
-	//Text,
-	Maths,
-	Mathsnew,
-	maths,
-	Random,
-	nCr,
-	nPr,
-	Pow,
-	Repetition,
-	Gcd,
-	Ratio,
-	Diagonal,
-	Zeller,
-	Push,
-	Com,
-	Spt,
-	Window,
-	UTF,
-	Day,
-	Css,
-	//Button,
-	Aimg,
-	//CSS,
-	Search,
-	ajax,
-	Time,
-	Html,
-	Code
-	//FCC_jQuery
+    sleep,
+    hello,
+    Modal_simple,
+    Accordion,
+    Thedetails,
+    //Text,
+    Maths,
+    Mathsnew,
+    maths,
+    Random,
+    nCr,
+    nPr,
+    Pow,
+    Repetition,
+    Gcd,
+    Ratio,
+    Diagonal,
+    Zeller,
+    Push,
+    Com,
+    Spt,
+    Window,
+    UTF,
+    Day,
+    Css,
+    //Button,
+    Aimg,
+    //CSS,
+    Search,
+    ajax,
+    Time,
+    Html,
+    Code,
+    birthday
+    //FCC_jQuery
 };
 //α version
